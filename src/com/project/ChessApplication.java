7742a7683763a6b2bee7d63fce4f8534d56ad196
Phillip Marques <phillip.silva@capgemini.com>
2022-07-04 17:56:46 -0300

    Moving pieces

--- src/com/project/ChessApplication.java
+++ src/com/project/ChessApplication.java
@@ -1,13 +1,29 @@
 package com.project;
 
+import java.util.Scanner;
+
 import com.project.chess.ChessMatch;
+import com.project.chess.ChessPiece;
+import com.project.chess.ChessPosition;
 
 public class ChessApplication {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner (System.in);
 		ChessMatch chessMatch = new ChessMatch();
+		
+		while(true) {
 		UI.printBoard(chessMatch.getPieces());
-
+		System.out.println();
+		System.out.println("Source: ");
+		ChessPosition source = UI.readChessPosition(sc);
+		
+		System.out.println();
+		System.out.println("Target: ");
+		ChessPosition target =  UI.readChessPosition(sc);
+		
+		ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		}
 	}
-
 }
--- src/com/project/chess/ChessException.java
+++ src/com/project/chess/ChessException.java
@@ -1,6 +1,8 @@
 package com.project.chess;
 
-public class ChessException extends RuntimeException {
+import com.project.boardgame.BoardException;
+
+public class ChessException extends BoardException {
 	
 	private static final long serialVersionUID = 1L;
 

2a9c6e632b832c2c9649b1dd0764989fea2c4172
Phillip Marques <79226575+pHMqZ@users.noreply.github.com>
2022-07-04 18:14:25 -0300

    Handling exceptions (#3)
    * initial commit
    
    * Improvement in board printing
    
    * Moving pieces
    
    * Handling exceptions and clearing screen

--- src/com/project/chess/ChessException.java
+++ src/com/project/chess/ChessException.java
@@ -1,7 +1,9 @@
 package com.project.chess;
 
-public class ChessException extends RuntimeException {
-	
+import com.project.boardgame.BoardException;
+
+public class ChessException extends BoardException {
+
 	private static final long serialVersionUID = 1L;
 
 	public ChessException(String msg) {

39611bc5707e8785f3ef9820cbcd723637c00bb2
Phillip Marques <phillipmarq@hotmail.com>
2022-07-12 23:02:53 -0300

    Promotion

--- src/com/project/Application.java
+++ src/com/project/Application.java
@@ -12,12 +12,12 @@ import com.project.chess.ChessPosition;
 
 public class Application {
 
-	public static void main(String[] args) {
-
+public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
-
+		
 		while (!chessMatch.getCheckMate()) {
 			try {
 				UI.clearScreen();
@@ -25,18 +25,24 @@ public class Application {
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
-
+				
 				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
 				UI.clearScreen();
 				UI.printBoard(chessMatch.getPieces(), possibleMoves);
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
-
+				
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-
+				
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
+				}
+				
+				if (chessMatch.getPromoted() != null) {
+					System.out.print("Enter piece for promotion (B/N/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
 				}
 			}
 			catch (ChessException e) {

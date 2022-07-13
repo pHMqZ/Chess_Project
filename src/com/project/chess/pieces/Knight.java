19c9c9ae79102202db48c592153244ae03371a25
Phillip Marques <phillipmarq@hotmail.com>
2022-07-12 22:58:57 -0300

    En Passant

--- src/com/project/chess/pieces/Knight.java
+++ src/com/project/chess/pieces/Knight.java
@@ -6,7 +6,7 @@ import com.project.chess.ChessPiece;
 import com.project.chess.Color;
 
 public class Knight extends ChessPiece {
-	
+
 	public Knight(Board board, Color color) {
 		super(board, color);
 	}
@@ -17,16 +17,16 @@ public class Knight extends ChessPiece {
 	}
 
 	private boolean canMove(Position position) {
-		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		ChessPiece p = (ChessPiece) getBoard().piece(position);
 		return p == null || p.getColor() != getColor();
 	}
-	
+
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
-		
+
 		Position p = new Position(0, 0);
-		
+
 		p.setValues(position.getRow() - 1, position.getColumn() - 2);
 		if (getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
@@ -61,7 +61,7 @@ public class Knight extends ChessPiece {
 		if (getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
-		
+
 		p.setValues(position.getRow() + 1, position.getColumn() - 2);
 		if (getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
